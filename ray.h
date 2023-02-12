0a708a174034cf8a0a660a6265b92e3da94354ea
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2023-02-12 18:35:48 +0100

    Allow for separate object definitions, object list

--- ray.h
+++ ray.h
@@ -4,6 +4,7 @@
 #include "ppm.h"
 #include "vec3.h"
 #include "viewport.h"
+#include "object.h"
 
 struct ray {
     struct vec3 orig;
@@ -13,7 +14,6 @@ struct ray {
 void ray_init(struct ray* ray, struct vec3 orig, struct vec3 dir);
 void ray_copy(struct ray* dest, const struct ray* src);
 struct vec3 ray_at(const struct ray* ray, double t);
-double ray_hit_sphere(struct vec3 center, double radius, const struct ray* r);
 struct vec3 ray_color(const struct ray* ray);
 void ray_cast(struct ray* ray, int xpos, int ypos);
 
